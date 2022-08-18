ca1110e152af6ae3d93fa0c947932090411a5479
lbn <lbn13600358463@163.com>
2022-08-18 23:45:18 +0800

    hotfix commit1

--- src/main/java/com/web/git/GitTest.java
+++ src/main/java/com/web/git/GitTest.java
@@ -7,5 +7,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
